bc845233853c4c6b1897292d34392409d6f0ea54
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-05 00:59:56 +0600

    Bind combobox with sql DB

---
+++ Bind_ComboBox_SQL_DB_WindowsApp/Bind_ComboBox_SQL_DB_WindowsApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Bind_ComboBox_SQL_DB_WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxEmployeeName = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxEmployeeName
+            // 
+            this.comboBoxEmployeeName.FormattingEnabled = true;
+            this.comboBoxEmployeeName.Location = new System.Drawing.Point(85, 174);
+            this.comboBoxEmployeeName.Name = "comboBoxEmployeeName";
+            this.comboBoxEmployeeName.Size = new System.Drawing.Size(270, 24);
+            this.comboBoxEmployeeName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxEmployeeName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxEmployeeName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Bind_ComboBox_SQL_DB_WindowsApp/Bind_ComboBox_SQL_DB_WindowsApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Bind_ComboBox_SQL_DB_WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBConnectionstring"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void BindCombobox()
+        {
+            SqlConnection con =  new SqlConnection(cs);
+            string query = "select * from Employee_Tbl";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                string name = dr.GetString(1);
+                comboBoxEmployeeName.Items.Add(name);
+            }
+
+            con.Close();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindCombobox();
+        }
+    }
+}
