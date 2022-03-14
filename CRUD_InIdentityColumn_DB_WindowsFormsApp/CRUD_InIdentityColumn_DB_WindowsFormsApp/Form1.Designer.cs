e5a2093c73b951e1c1914ffcfe1e018c64958c6a
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-15 00:00:53 +0600

    CRUD Operation with identity column with SQL DB

---
+++ CRUD_InIdentityColumn_DB_WindowsFormsApp/CRUD_InIdentityColumn_DB_WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace CRUD_InIdentityColumn_DB_WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCompany = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxCategory = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxStock = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridViewProductDeatils = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProductDeatils)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(294, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Deatils";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(231, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name: ";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(366, 92);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(222, 22);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(231, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Company: ";
+            // 
+            // textBoxCompany
+            // 
+            this.textBoxCompany.Location = new System.Drawing.Point(366, 136);
+            this.textBoxCompany.Name = "textBoxCompany";
+            this.textBoxCompany.Size = new System.Drawing.Size(222, 22);
+            this.textBoxCompany.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(231, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Category: ";
+            // 
+            // textBoxCategory
+            // 
+            this.textBoxCategory.Location = new System.Drawing.Point(366, 180);
+            this.textBoxCategory.Name = "textBoxCategory";
+            this.textBoxCategory.Size = new System.Drawing.Size(222, 22);
+            this.textBoxCategory.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(231, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 23);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Price: ";
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.Location = new System.Drawing.Point(366, 224);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(222, 22);
+            this.textBoxPrice.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(231, 276);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 23);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Stock: ";
+            // 
+            // textBoxStock
+            // 
+            this.textBoxStock.Location = new System.Drawing.Point(366, 276);
+            this.textBoxStock.Name = "textBoxStock";
+            this.textBoxStock.Size = new System.Drawing.Size(222, 22);
+            this.textBoxStock.TabIndex = 4;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(178, 337);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(100, 36);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(366, 337);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(119, 36);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(562, 337);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(125, 36);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridViewProductDeatils
+            // 
+            this.dataGridViewProductDeatils.AllowUserToAddRows = false;
+            this.dataGridViewProductDeatils.AllowUserToDeleteRows = false;
+            this.dataGridViewProductDeatils.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProductDeatils.Location = new System.Drawing.Point(61, 399);
+            this.dataGridViewProductDeatils.Name = "dataGridViewProductDeatils";
+            this.dataGridViewProductDeatils.ReadOnly = true;
+            this.dataGridViewProductDeatils.RowHeadersWidth = 51;
+            this.dataGridViewProductDeatils.RowTemplate.Height = 24;
+            this.dataGridViewProductDeatils.Size = new System.Drawing.Size(755, 311);
+            this.dataGridViewProductDeatils.TabIndex = 6;
+            this.dataGridViewProductDeatils.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewProductDeatils_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 729);
+            this.Controls.Add(this.dataGridViewProductDeatils);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.textBoxStock);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxCompany);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxCategory);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Product Details";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProductDeatils)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCompany;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxCategory;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxStock;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridViewProductDeatils;
+    }
+}
+
---
+++ CRUD_InIdentityColumn_DB_WindowsFormsApp/CRUD_InIdentityColumn_DB_WindowsFormsApp/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace CRUD_InIdentityColumn_DB_WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        int id;
+        string cs = ConfigurationManager.ConnectionStrings["DBConnectionstring"].ConnectionString; 
+        public Form1()
+        {
+            InitializeComponent();
+            BindGridView();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+            string queryInsert = "insert into Product_Tbl values(@name, @company, @category,@price,@stock)";
+
+            SqlCommand cmd = new SqlCommand(queryInsert, con);
+
+            cmd.Parameters.AddWithValue("@name",textBoxName.Text);
+            cmd.Parameters.AddWithValue("@company", textBoxCompany.Text);
+            cmd.Parameters.AddWithValue("@category", textBoxCategory.Text);
+            cmd.Parameters.AddWithValue("@price",textBoxPrice.Text);
+            cmd.Parameters.AddWithValue("@stock", textBoxStock.Text);
+
+            con.Open();
+
+            int value =  cmd.ExecuteNonQuery();
+
+            if (value> 0)
+            {
+                MessageBox.Show("Inserted successfuly", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                BindGridView();
+            }
+
+            else
+            {
+                MessageBox.Show("Inserted failed", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+            con.Close();
+
+        }
+
+        void BindGridView()
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+            string querySelect = "select * from Product_Tbl";
+
+            // SqlCommand cmd = new SqlCommand(querySelect, con);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(querySelect, con);
+
+            DataTable dataTable = new DataTable();
+
+            adapter.Fill(dataTable);
+
+            dataGridViewProductDeatils.DataSource = dataTable;
+
+            dataGridViewProductDeatils.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+            string queryInsert = "update Product_Tbl set Product_Name = @name, Product_Company = @company, Product_Category = @category, Product_Price = @price, Product_Stock = @stock where Product_Id = @productId";
+
+            SqlCommand cmd = new SqlCommand(queryInsert, con);
+
+            cmd.Parameters.AddWithValue("@productId", id);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@company", textBoxCompany.Text);
+            cmd.Parameters.AddWithValue("@category", textBoxCategory.Text);
+            cmd.Parameters.AddWithValue("@price", textBoxPrice.Text);
+            cmd.Parameters.AddWithValue("@stock", textBoxStock.Text);
+
+            con.Open();
+
+            int value = cmd.ExecuteNonQuery();
+
+            if (value > 0)
+            {
+                MessageBox.Show("Updated successfuly", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                BindGridView();
+            }
+
+            else
+            {
+                MessageBox.Show("Updated failed", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+            con.Close();
+        }
+
+        private void dataGridViewProductDeatils_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            id =Convert.ToInt32 ( dataGridViewProductDeatils.SelectedRows[0].Cells[0].Value);
+            textBoxName.Text = dataGridViewProductDeatils.SelectedRows[0].Cells[1].Value.ToString();
+            textBoxCompany.Text = dataGridViewProductDeatils.SelectedRows[0].Cells[2].Value.ToString();
+            textBoxCategory.Text = dataGridViewProductDeatils.SelectedRows[0].Cells[3].Value.ToString();
+            textBoxPrice.Text = dataGridViewProductDeatils.SelectedRows[0].Cells[4].Value.ToString();
+            textBoxStock.Text = dataGridViewProductDeatils.SelectedRows[0].Cells[5].Value.ToString();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+            string queryInsert = "delete from Product_Tbl where Product_Id = @productId";
+
+            SqlCommand cmd = new SqlCommand(queryInsert, con);
+
+            cmd.Parameters.AddWithValue("@productId", id);
+         
+
+            con.Open();
+
+            int value = cmd.ExecuteNonQuery();
+
+            if (value > 0)
+            {
+                MessageBox.Show("Deleted successfuly", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                BindGridView();
+            }
+
+            else
+            {
+                MessageBox.Show("Deleted failed", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
+            con.Close();
+        }
+    }
+}
