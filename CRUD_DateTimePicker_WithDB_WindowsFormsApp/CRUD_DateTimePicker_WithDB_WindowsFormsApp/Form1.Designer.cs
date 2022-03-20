6fedce1d3e4d7ef76293bb4304a9aa941ba49957
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-20 21:13:55 +0600

    CRUD operation with DB using Datetime picker

---
+++ CRUD_DateTimePicker_WithDB_WindowsFormsApp/CRUD_DateTimePicker_WithDB_WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace CRUD_DateTimePicker_WithDB_WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dateTimePickerDOJ = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerTOJ = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerDOB = new System.Windows.Forms.DateTimePicker();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridViewDateTimeShowList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDateTimeShowList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(601, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Insert, Update, Delete In DB Using Datetime picker";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(238, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID: ";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(282, 84);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(250, 22);
+            this.textBoxId.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(207, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Name: ";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(282, 124);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(250, 22);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(153, 165);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 22);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Date Of Join: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(151, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 22);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Time Of Join: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(148, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 22);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Date Of Birth: ";
+            // 
+            // dateTimePickerDOJ
+            // 
+            this.dateTimePickerDOJ.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerDOJ.Location = new System.Drawing.Point(282, 164);
+            this.dateTimePickerDOJ.Name = "dateTimePickerDOJ";
+            this.dateTimePickerDOJ.Size = new System.Drawing.Size(250, 22);
+            this.dateTimePickerDOJ.TabIndex = 2;
+            // 
+            // dateTimePickerTOJ
+            // 
+            this.dateTimePickerTOJ.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerTOJ.Location = new System.Drawing.Point(283, 207);
+            this.dateTimePickerTOJ.Name = "dateTimePickerTOJ";
+            this.dateTimePickerTOJ.ShowUpDown = true;
+            this.dateTimePickerTOJ.Size = new System.Drawing.Size(250, 22);
+            this.dateTimePickerTOJ.TabIndex = 3;
+            // 
+            // dateTimePickerDOB
+            // 
+            this.dateTimePickerDOB.CustomFormat = "dd-MM-yyyy hh:mm:ss tt";
+            this.dateTimePickerDOB.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerDOB.Location = new System.Drawing.Point(282, 251);
+            this.dateTimePickerDOB.Name = "dateTimePickerDOB";
+            this.dateTimePickerDOB.Size = new System.Drawing.Size(250, 22);
+            this.dateTimePickerDOB.TabIndex = 4;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(621, 82);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(126, 47);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(621, 152);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(126, 47);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(621, 226);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(126, 47);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // dataGridViewDateTimeShowList
+            // 
+            this.dataGridViewDateTimeShowList.AllowUserToAddRows = false;
+            this.dataGridViewDateTimeShowList.AllowUserToDeleteRows = false;
+            this.dataGridViewDateTimeShowList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDateTimeShowList.Location = new System.Drawing.Point(68, 315);
+            this.dataGridViewDateTimeShowList.Name = "dataGridViewDateTimeShowList";
+            this.dataGridViewDateTimeShowList.ReadOnly = true;
+            this.dataGridViewDateTimeShowList.RowHeadersWidth = 51;
+            this.dataGridViewDateTimeShowList.RowTemplate.Height = 24;
+            this.dataGridViewDateTimeShowList.Size = new System.Drawing.Size(806, 348);
+            this.dataGridViewDateTimeShowList.TabIndex = 5;
+            this.dataGridViewDateTimeShowList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewDateTimeShowList_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 691);
+            this.Controls.Add(this.dataGridViewDateTimeShowList);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dateTimePickerDOB);
+            this.Controls.Add(this.dateTimePickerTOJ);
+            this.Controls.Add(this.dateTimePickerDOJ);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Datetime picker";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDateTimeShowList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDOJ;
+        private System.Windows.Forms.DateTimePicker dateTimePickerTOJ;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDOB;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridViewDateTimeShowList;
+    }
+}
+
---
+++ CRUD_DateTimePicker_WithDB_WindowsFormsApp/CRUD_DateTimePicker_WithDB_WindowsFormsApp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace CRUD_DateTimePicker_WithDB_WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBConnectionstring"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+            BindGridview();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con =  new SqlConnection(cs);
+            string queryInsert = "insert into DateTimePicker_Tbl values(@id, @name,@doj,@toj,@dob)";
+
+            SqlCommand cmd =  new SqlCommand(queryInsert, con);
+
+            cmd.Parameters.AddWithValue("@id", textBoxId.Text);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@doj", dateTimePickerDOJ.Value);
+            cmd.Parameters.AddWithValue("@toj", dateTimePickerTOJ.Value);
+            cmd.Parameters.AddWithValue("@dob", dateTimePickerDOB.Value);
+
+            con.Open();
+            int value =  cmd.ExecuteNonQuery();
+
+            if (value > 0)
+            {
+                MessageBox.Show("Success");
+
+                BindGridview();
+
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+            con.Close();
+        }
+
+
+        void BindGridview()
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+            string querySelect = "Select * from DateTimePicker_Tbl ";
+
+            SqlDataAdapter adapter =  new SqlDataAdapter(querySelect, con);
+
+            DataTable dataTable = new DataTable();
+
+            adapter.Fill(dataTable);
+            dataGridViewDateTimeShowList.DataSource = dataTable; 
+
+
+        }
+
+        private void dataGridViewDateTimeShowList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBoxId.Text =  dataGridViewDateTimeShowList.SelectedRows[0].Cells[0].Value.ToString();
+            textBoxName.Text =  dataGridViewDateTimeShowList.SelectedRows[0].Cells[1].Value.ToString();
+            dateTimePickerDOJ.Value =Convert.ToDateTime( dataGridViewDateTimeShowList.SelectedRows[0].Cells[2].Value);
+            dateTimePickerTOJ.Value =Convert.ToDateTime( dataGridViewDateTimeShowList.SelectedRows[0].Cells[3].Value);
+            dateTimePickerDOB.Value =Convert.ToDateTime( dataGridViewDateTimeShowList.SelectedRows[0].Cells[4].Value);
+        }
+    }
+}
