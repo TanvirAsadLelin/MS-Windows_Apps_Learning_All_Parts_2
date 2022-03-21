f46fa9f3fcc79fd4871f9603514c243ed7f237d2
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-21 18:10:54 +0600

    Auto increment textbox value with DB Appliacation

---
+++ AutoIncrementTextboxValue_WithDB/AutoIncrementTextboxValue_WithDB_WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace AutoIncrementTextboxValue_WithDB_WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridViewList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(234, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto increment textbox value with DB";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(310, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID: ";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxID.Location = new System.Drawing.Point(358, 60);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(161, 27);
+            this.textBoxID.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(277, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Name: ";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxName.Location = new System.Drawing.Point(358, 103);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(161, 27);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(293, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Age: ";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAge.Location = new System.Drawing.Point(358, 146);
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(161, 27);
+            this.textBoxAge.TabIndex = 2;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInsert.Location = new System.Drawing.Point(179, 199);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(91, 36);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(378, 199);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(91, 36);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(558, 199);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(91, 36);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridViewList
+            // 
+            this.dataGridViewList.AllowUserToAddRows = false;
+            this.dataGridViewList.AllowUserToDeleteRows = false;
+            this.dataGridViewList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewList.Location = new System.Drawing.Point(91, 272);
+            this.dataGridViewList.Name = "dataGridViewList";
+            this.dataGridViewList.ReadOnly = true;
+            this.dataGridViewList.RowHeadersWidth = 51;
+            this.dataGridViewList.RowTemplate.Height = 24;
+            this.dataGridViewList.Size = new System.Drawing.Size(671, 260);
+            this.dataGridViewList.TabIndex = 6;
+            this.dataGridViewList.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridViewList_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 564);
+            this.Controls.Add(this.dataGridViewList);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridViewList;
+    }
+}
+
---
+++ AutoIncrementTextboxValue_WithDB/AutoIncrementTextboxValue_WithDB_WindowsFormsApp/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AutoIncrementTextboxValue_WithDB_WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBConnectionstring"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+            BindGridview();
+        }
+
+        void GetIncrementId()
+        {
+            SqlConnection con =  new SqlConnection(cs);
+
+            string querySelect = "select Id from List_Tbl";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(querySelect, con);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            if(dataTable.Rows.Count < 1)
+            {
+                textBoxID.Text = "1";
+            }
+            else
+            {
+                SqlConnection con1 = new SqlConnection(cs);
+                string querySelect_1 = "select max(Id) from List_Tbl";
+
+                SqlCommand cmd = new SqlCommand(querySelect_1, con1);
+                con1.Open();
+                int value =  Convert.ToInt32(cmd.ExecuteScalar());
+                con1.Close();
+
+                value = value + 1;
+                textBoxID.Text = value.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetIncrementId();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection con2 = new SqlConnection(cs);
+            string queryInsert = "insert into List_Tbl values (@id,@name,@age)"; 
+
+            SqlCommand cmd = new SqlCommand(queryInsert, con2);
+            cmd.Parameters.AddWithValue("@id", textBoxID.Text);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@age", textBoxAge.Text);
+            
+            con2.Open();
+            int value = cmd.ExecuteNonQuery();
+            if(value > 0)
+            {
+                MessageBox.Show("Inserted");
+                GetIncrementId();
+                textBoxName.Text = "";
+                textBoxAge.Text = "";
+                BindGridview();
+            }
+            else
+            {
+                MessageBox.Show("Failed");
+            }
+            con2.Close();
+        }
+
+
+        void BindGridview()
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+            string querySelect = "select * from List_Tbl ";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(querySelect, con);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            dataGridViewList.DataSource = dataTable;
+            dataGridViewList.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+        }
+
+        private void dataGridViewList_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            textBoxID.Text = dataGridViewList.SelectedRows[0].Cells[0].Value.ToString();
+            textBoxName.Text = dataGridViewList.SelectedRows[0].Cells[1].Value.ToString();
+            textBoxAge.Text = dataGridViewList.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con2 = new SqlConnection(cs);
+            string queryInsert = "update List_Tbl set Name=@name,Age = @age where Id=@id";
+
+            SqlCommand cmd = new SqlCommand(queryInsert, con2);
+            cmd.Parameters.AddWithValue("@id", textBoxID.Text);
+            cmd.Parameters.AddWithValue("@name", textBoxName.Text);
+            cmd.Parameters.AddWithValue("@age", textBoxAge.Text);
+
+            con2.Open();
+            int value = cmd.ExecuteNonQuery();
+            if (value > 0)
+            {
+                MessageBox.Show("Updated");
+                GetIncrementId();
+                textBoxName.Text = "";
+                textBoxAge.Text = "";
+                BindGridview();
+            }
+            else
+            {
+                MessageBox.Show("Update fail");
+            }
+            con2.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con2 = new SqlConnection(cs);
+            string queryInsert = "delete from List_Tbl where Id=@id";
+
+            SqlCommand cmd = new SqlCommand(queryInsert, con2);
+            cmd.Parameters.AddWithValue("@id", textBoxID.Text);
+            con2.Open();
+            int value = cmd.ExecuteNonQuery();
+            if (value > 0)
+            {
+                MessageBox.Show("Deleted");
+                GetIncrementId();
+                textBoxName.Text = "";
+                textBoxAge.Text = "";
+                BindGridview();
+            }
+            else
+            {
+                MessageBox.Show("Delete fail");
+            }
+            con2.Close();
+        }
+    }
+}
