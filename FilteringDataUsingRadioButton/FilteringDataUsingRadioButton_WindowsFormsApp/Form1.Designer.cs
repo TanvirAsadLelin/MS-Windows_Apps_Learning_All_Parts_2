02661a6c087f8da6dea9c3dafd2a51e4c7adbf19
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-03-21 12:15:02 +0600

    Filtering data from DB using Radio button with data gridview Appliacation

---
+++ FilteringDataUsingRadioButton/FilteringDataUsingRadioButton_WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace FilteringDataUsingRadioButton_WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonMale = new System.Windows.Forms.RadioButton();
+            this.radioButtonFemale = new System.Windows.Forms.RadioButton();
+            this.dataGridViewEmployee = new System.Windows.Forms.DataGridView();
+            this.radioButtonBoth = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployee)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.groupBox1.Controls.Add(this.radioButtonBoth);
+            this.groupBox1.Controls.Add(this.radioButtonFemale);
+            this.groupBox1.Controls.Add(this.radioButtonMale);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(311, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(336, 196);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search by gender";
+            // 
+            // radioButtonMale
+            // 
+            this.radioButtonMale.AutoSize = true;
+            this.radioButtonMale.Location = new System.Drawing.Point(29, 46);
+            this.radioButtonMale.Name = "radioButtonMale";
+            this.radioButtonMale.Size = new System.Drawing.Size(87, 33);
+            this.radioButtonMale.TabIndex = 1;
+            this.radioButtonMale.TabStop = true;
+            this.radioButtonMale.Text = "Male";
+            this.radioButtonMale.UseVisualStyleBackColor = true;
+            this.radioButtonMale.CheckedChanged += new System.EventHandler(this.radioButtonMale_CheckedChanged);
+            // 
+            // radioButtonFemale
+            // 
+            this.radioButtonFemale.AutoSize = true;
+            this.radioButtonFemale.Location = new System.Drawing.Point(29, 95);
+            this.radioButtonFemale.Name = "radioButtonFemale";
+            this.radioButtonFemale.Size = new System.Drawing.Size(116, 33);
+            this.radioButtonFemale.TabIndex = 2;
+            this.radioButtonFemale.TabStop = true;
+            this.radioButtonFemale.Text = "Female";
+            this.radioButtonFemale.UseVisualStyleBackColor = true;
+            this.radioButtonFemale.CheckedChanged += new System.EventHandler(this.radioButtonFemale_CheckedChanged);
+            // 
+            // dataGridViewEmployee
+            // 
+            this.dataGridViewEmployee.AllowUserToAddRows = false;
+            this.dataGridViewEmployee.AllowUserToDeleteRows = false;
+            this.dataGridViewEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewEmployee.Location = new System.Drawing.Point(17, 224);
+            this.dataGridViewEmployee.Name = "dataGridViewEmployee";
+            this.dataGridViewEmployee.ReadOnly = true;
+            this.dataGridViewEmployee.RowHeadersWidth = 51;
+            this.dataGridViewEmployee.RowTemplate.Height = 24;
+            this.dataGridViewEmployee.Size = new System.Drawing.Size(942, 381);
+            this.dataGridViewEmployee.TabIndex = 0;
+            // 
+            // radioButtonBoth
+            // 
+            this.radioButtonBoth.AutoSize = true;
+            this.radioButtonBoth.Location = new System.Drawing.Point(29, 143);
+            this.radioButtonBoth.Name = "radioButtonBoth";
+            this.radioButtonBoth.Size = new System.Drawing.Size(83, 33);
+            this.radioButtonBoth.TabIndex = 2;
+            this.radioButtonBoth.TabStop = true;
+            this.radioButtonBoth.Text = "Both";
+            this.radioButtonBoth.UseVisualStyleBackColor = true;
+            this.radioButtonBoth.CheckedChanged += new System.EventHandler(this.radioButtonBoth_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 632);
+            this.Controls.Add(this.dataGridViewEmployee);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewEmployee)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonFemale;
+        private System.Windows.Forms.RadioButton radioButtonMale;
+        private System.Windows.Forms.DataGridView dataGridViewEmployee;
+        private System.Windows.Forms.RadioButton radioButtonBoth;
+    }
+}
+
---
+++ FilteringDataUsingRadioButton/FilteringDataUsingRadioButton_WindowsFormsApp/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace FilteringDataUsingRadioButton_WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["DBConnectionstring"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+            BindGridView();
+        }
+
+        void BindGridView()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string querySelect = "select * from Employee_Tbl";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(querySelect, con);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            dataGridViewEmployee.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewEmployee.DataSource = dataTable;
+
+
+
+            // For specific Coloumn get in Data gridview
+
+
+            //dataGridViewEmployee.Rows.Clear();
+            //foreach(DataRow row in dataTable.Rows)
+            //{
+            //    int index  =  dataGridViewEmployee.Rows.Add();
+
+            //    dataGridViewEmployee.Rows[index].Cells[0].Value = row["Id"].ToString();
+            //    dataGridViewEmployee.Rows[index].Cells[1].Value = row["Name"].ToString();
+            //}
+
+
+        }
+
+        private void radioButtonMale_CheckedChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string querySelect = "select * from Employee_Tbl where Gender = 'Male' ";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(querySelect, con);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridViewEmployee.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewEmployee.DataSource = dataTable;
+        }
+
+        private void radioButtonFemale_CheckedChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string querySelect = "select * from Employee_Tbl where Gender = 'Female' ";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(querySelect, con);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridViewEmployee.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewEmployee.DataSource = dataTable;
+        }
+
+        private void radioButtonBoth_CheckedChanged(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string querySelect = "select * from Employee_Tbl";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(querySelect, con);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+            dataGridViewEmployee.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewEmployee.DataSource = dataTable;
+        }
+
+
+
+    }
+}
